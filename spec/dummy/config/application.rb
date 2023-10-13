546991df36391540938e7d31631801bbf41aff75
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2023-10-13 11:35:21 +0900

    Test on rails 7.1 and postgres 15

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -8,6 +8,8 @@ require "activerecord_scoping_with_assoc"
 
 module Dummy
   class Application < Rails::Application
+    config.load_defaults Rails::VERSION::STRING.to_f
+
     unless (database = ENV['DATABASE'].to_s).empty?
       config.paths["config/database"] = "config/database_#{database}.yml"
       ENV['SCHEMA'] = Rails.root.join("db/schema_#{database}.rb").to_s
